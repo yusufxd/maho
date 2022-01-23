5e39675d1c3411f004a4647aba8d4a98d185436b
kuralsiz47 <98223394+kuralsiz47@users.noreply.github.com>
2022-01-23 21:39:27 +0300

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -10,27 +10,27 @@ def PermissionCheck(mystic):
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "I need to be admin with some permissions:\n"
-                + "\n- **can_manage_voice_chats:** To manage voice chats"
-                + "\n- **can_delete_messages:** To delete Bot's Searched Waste"
-                + "\n- **can_invite_users**: For inviting assistant to chat."
+                "Bazı izinlere sahip yönetici olmam gerekiyor:\n"
+                + "\n- **SESLİ SOHBETLERİ YÖNET:** Sesli sohbetleri yönetmek için"
+                + "\n- **MESAJLARI SİL:** Botun Aranan Atıklarını silmek için"
+                + "\n- **BAĞLANTI YOLUYLA DAVET ET**: Asistanı sohbete davet etmek için."
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __MANAGE VOICE CHATS__"
+                "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
+                + "\n**İzin:** __SESLİ SOHBETLERİ YÖNET__"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __DELETE MESSAGES__"
+                "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
+                + "\n**İzin:** __MESAJLARI SİL__"
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
-                "I don't have the required permission to perform this action."
-                + "\n**Permission:** __INVITE USERS VIA LINK__"
+                "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
+                + "\n**İzin:** __KULLANICILARI BAĞLANTI YOLUYLA DAVET ET__"
             )
             return
         return await mystic(_, message)
