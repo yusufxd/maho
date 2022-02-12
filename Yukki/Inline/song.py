e4cb56429c4ec35781f07df39dfd670e830e2287
Efsanestar <83150866+Mehmetbaba06@users.noreply.github.com>
2022-02-12 12:30:47 +0300

    Update song.py

--- Yukki/Inline/song.py
+++ Yukki/Inline/song.py
@@ -20,7 +20,7 @@ def song_markup(videoid, duration, user_id, query, query_type):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Aramayı Kapat",
+                text="❌ Çıkış",
                 callback_data=f"forceclose {query}|{user_id}",
             )
         ],
@@ -42,7 +42,7 @@ def song_download_markup(videoid, user_id):
         ],
         [
             InlineKeyboardButton(
-                text="🗑 Aramayı Kapat",
+                text="❌ Çıkış",
                 callback_data=f"forceclose {videoid}|{user_id}",
             )
         ],
