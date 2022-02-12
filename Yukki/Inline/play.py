32fdb7afc799c3f9d6d81832c320b34fa4e7d567
Efsanestar <83150866+Mehmetbaba06@users.noreply.github.com>
2022-02-12 18:37:38 +0300

    Update play.py

--- Yukki/Inline/play.py
+++ Yukki/Inline/play.py
@@ -16,7 +16,7 @@ def url_markup(videoid, duration, user_id, query, query_type):
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="📽️",
+                text="📹",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
@@ -46,7 +46,7 @@ def url_markup2(videoid, duration, user_id):
                 callback_data=f"MusicStream {videoid}|{duration}|{user_id}",
             ),
             InlineKeyboardButton(
-                text="🎥 Video",
+                text="📹 Video",
                 callback_data=f"Choose {videoid}|{duration}|{user_id}",
             ),
         ],
