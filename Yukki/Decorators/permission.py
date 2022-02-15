e03bd8bbbc756d51906344ade2205d2c596167d7
Efsanestar <83150866+Mehmetbaba06@users.noreply.github.com>
2022-02-15 09:19:14 +0300

    Update permission.py

--- Yukki/Decorators/permission.py
+++ Yukki/Decorators/permission.py
@@ -5,32 +5,30 @@ from Yukki import BOT_ID, app
 
 def PermissionCheck(mystic):
     async def wrapper(_, message):
-        if message.chat.type == "private":
-            return await mystic(_, message)
         a = await app.get_chat_member(message.chat.id, BOT_ID)
         if a.status != "administrator":
             return await message.reply_text(
-                "Bazı izinlere sahip yönetici olmam gerekiyor:\n"
-                + "\n- **SESLİ SOHBETLERİ YÖNET:** Sesli sohbetleri yönetmek için"
-                + "\n- **MESAJLARI SİL:** Botun Aranan Atıklarını silmek için"
-                + "\n- **BAĞLANTI YOLUYLA DAVET ET**: Asistanı sohbete davet etmek için."
+                "Bazı izinlerle yönetici olmam gerekiyor:\n"
+                + "\n- **can_manage_voice_chats:** Sesli sohbetleri yönetmek için"
+                + "\n- **can_delete_messages:** Botların Aradığı Atıkları silmek için"
+                + "\n- **can_invite_users**: Asistanı sohbete davet etmek için."
             )
         if not a.can_manage_voice_chats:
             await message.reply_text(
                 "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
-                + "\n**İzin:** __SESLİ SOHBETLERİ YÖNET__"
+                + "\n**Permission:** __SESLİ SOHBETLERİ YÖNETİN__"
             )
             return
         if not a.can_delete_messages:
             await message.reply_text(
                 "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
-                + "\n**İzin:** __MESAJLARI SİL__"
+                + "\n**Permission:** __MESAJLARI SİL__"
             )
             return
         if not a.can_invite_users:
             await message.reply_text(
                 "Bu eylemi gerçekleştirmek için gerekli izne sahip değilim."
-                + "\n**İzin:** __KULLANICILARI BAĞLANTI YOLUYLA DAVET ET__"
+                + "\n**Permission:** __BAĞLANTI YOLUYLA KULLANICILARI DAVET ET__"
             )
             return
         return await mystic(_, message)
