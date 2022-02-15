5de395d3a9f0dd2becc0f661f68759c66361858f
Efsanestar <83150866+Mehmetbaba06@users.noreply.github.com>
2022-02-15 09:16:07 +0300

    Update checker.py

--- Yukki/Decorators/checker.py
+++ Yukki/Decorators/checker.py
@@ -6,22 +6,22 @@ def checker(mystic):
     async def wrapper(_, message):
         if message.sender_chat:
             return await message.reply_text(
-                "You're an __Anonymous Admin__ in this Chat Group!\nRevert back to User Account From Admin Rights."
+                "Bu Sohbet Grubunda __Anonim Yönetici__siniz!\nYönetici Haklarından Kullanıcı Hesabına Geri Dönün."
             )
         blacklisted_chats_list = await blacklisted_chats()
         if message.chat.id in blacklisted_chats_list:
             await message.reply_text(
-                f"**Blacklisted Chat**\n\nYour chat has been blacklisted by Sudo Users.Ask any __SUDO USER__ to whitelist.\nCheck Sudo Users List [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Kara Listeye Alınmış Sohbet**\n\nSohbetiniz Sudo Kullanıcıları tarafından kara listeye alındı. Herhangi bir __SUDO USER__'dan beyaz listeye alınmasını isteyin.\nSudo Kullanıcı Listesini Kontrol Edin [Buradan](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
             return await app.leave_chat(message.chat.id)
         if await is_on_off(1):
             if int(message.chat.id) != int(LOG_GROUP_ID):
                 return await message.reply_text(
-                    f"Bot is under Maintenance. Sorry for the inconvenience!"
+                    f"Bot Bakımda. rahatsızlıktan dolayı özür dileriz!"
                 )
         if await is_gbanned_user(message.from_user.id):
             return await message.reply_text(
-                f"**Gbanned User**\n\nYou're gbanned from using Bot.Ask any __SUDO USER__ to ungban.\nCheck Sudo Users List [From Here](https://t.me/{BOT_USERNAME}?start=sudolist)"
+                f"**Yasaklanan Kullanıcı**\n\nBot'u kullanmanız yasaklandı. Herhangi bir __SUDO USER__'dan destek alın.\nSudo Kullanıcı Listesini Kontrol Edin [Buradan](https://t.me/{BOT_USERNAME}?start=sudolist)"
             )
         return await mystic(_, message)
 
@@ -33,17 +33,17 @@ def checkerCB(mystic):
         blacklisted_chats_list = await blacklisted_chats()
         if CallbackQuery.message.chat.id in blacklisted_chats_list:
             return await CallbackQuery.answer(
-                "Blacklisted Chat", show_alert=True
+                "Kara Listeye Alınmış Sohbet", show_alert=True
             )
         if await is_on_off(1):
             if int(CallbackQuery.message.chat.id) != int(LOG_GROUP_ID):
                 return await CallbackQuery.answer(
-                    "Bot is under Maintenance. Sorry for the inconvenience!",
+                    "Bot Bakımda. rahatsızlıktan dolayı özür dileriz!",
                     show_alert=True,
                 )
         if await is_gbanned_user(CallbackQuery.from_user.id):
             return await CallbackQuery.answer(
-                "You're Gbanned", show_alert=True
+                "Global Banlısın", show_alert=True
             )
         return await mystic(_, CallbackQuery)
 
